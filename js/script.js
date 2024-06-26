89270a4273e54cacb81062e3ce9284605af582b3
saviita <taatt2000@gmail.com>
2024-06-26 09:59:57 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Declarar funciones
+
+/*let number = 0;
+
+   function addOne(){
+     number++;
+ }
+
+addOne();
+console.log(number);*/
+
+function sumNumber(numberA, numberB){
+    console.log(numberA + numberB);
+}
+
+function printName(name, age){
+    console.log('Hola me llamo ' + name + ' y tengo ' + age + ' años.');
+}
+
+sumNumber(4, 8);
+sumNumber(2, 6);
+sumNumber(5, 20);
+
+printName('Carlos', 23);
